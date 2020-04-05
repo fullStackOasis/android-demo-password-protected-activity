e5c963bac05636055d6547f635480e285bf43db0
fullStackOasis <fullstackdev@fullstackoasis.com>
2020-04-05 10:18:20 -0400

    In MainActivity, click to get to the SecretActivity.

--- app/src/main/java/com/fullstackoasis/passwordprotected/MainActivity.java
+++ app/src/main/java/com/fullstackoasis/passwordprotected/MainActivity.java
@@ -2,13 +2,27 @@ package com.fullstackoasis.passwordprotected;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+/**
+ * MainActivity has a text message and a Button. Clicking the button sends you to SecretActivity.
+ */
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecretActivity.class);
+                MainActivity.this.startActivity(i);
+            }
+        });
     }
 }
